b4f5c550d78811bb549ab0eeb3c762fba8ef7f6a
konika098 <konikaakter2000@gmail.com>
2023-11-24 04:40:40 +0600

    add services section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 
 import Banner from './Banner/Banner';
 import Cities from './Cities/Cities';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
        <>
        <Banner/>
        <Cities/>
+       <Services/>
        
        </>
     );
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,34 @@
+import  { useEffect, useState } from 'react';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(()=>{
+        fetch('Services.json')
+        .then(response => {
+            return response.json()
+           
+          })
+          .then(data => {
+            setServices(data);
+        
+            console.log(data)
+          })
+    },[])
+    return (
+      <>
+      <h1 className='text-4xl  text-[#004274] font-semibold mb-5 mt-16'>Our Services</h1>
+      <div className='grid grid-cols-3 gap-4'>
+                {services.map(service => (
+                    <div key={service.id}>
+                        <img className='w-12 h-12' src={service.icon} alt="" />
+                        <h2 className='text-xl text-[#004274] font-semibold mb-2 mt-2'>{service.serviceName}</h2>
+                        <p className='text-md mb-10'>{service.description}</p>
+                    </div>
+                ))}
+            </div>
+      
+      </>
+    );
+};
+
+export default Services;
